d072889af7fe0ffecae4465b7ea7a8e656aebe24
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-11-09 13:45:27 +0200

    models types with ts in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Todos from './Components/Todos';
 
+import Todo from './models/todo';
+
 
 function App() {
+
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn TypeScript'),
+  ]
+
   return (
     <div>
-      <Todos items={['Learn react', 'Learn Typescript']} />
+      <Todos items={todos} />
     </div>
   );
 }
